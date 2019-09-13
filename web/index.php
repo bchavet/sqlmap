7cc151c7a9ac571806f52d28312ccef287bd817f
Tugboat <root@tugboat.qa>
2019-09-13 03:46:05 +0000

    output data field

--- web/index.php
+++ web/index.php
@@ -3,11 +3,11 @@ $id = $_GET['id'];
 
 if ($id) {
     $mysqli = new mysqli("mysql", "tugboat", "tugboat", "tugboat");
-    $result = $mysqli->query("select * from demo where id=$id");
+    $result = $mysqli->query("select data from demo where id=$id");
     $rows = $result->num_rows;
 
-    if ($rows > 0) {
-	echo 'OK';
+    if ($rows == 1) {
+        echo $result->fetch_object()->data;
     } else {
 	http_response_code(404);
 	echo '404 - NOT FOUND';
