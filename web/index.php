35a95618a33b8e8b18cdbe1e2005cf543dab0c5e
Tugboat <root@tugboat.qa>
2019-09-13 03:38:21 +0000

    return 404 if not found

--- web/index.php
+++ web/index.php
@@ -4,8 +4,14 @@ $id = $_GET['id'];
 if ($id) {
     $mysqli = new mysqli("mysql", "tugboat", "tugboat", "tugboat");
     $result = $mysqli->query("select * from demo where id=$id");
-    
-    echo $result->num_rows;
+    $rows = $result->num_rows;
+
+    if ($rows > 0) {
+	echo 'OK';
+    } else {
+	http_response_code(404);
+	echo '404 - NOT FOUND';
+    }
 } else {
     echo '<a href="/index.php?id=1">GO</a>';
 }
